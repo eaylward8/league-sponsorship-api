0d75923a06cd1b9ef63eb7910ddb0a36275feeb8
Erik Aylward <eaylward@mail.med.upenn.edu>
2019-10-09 17:49:15 -0400

    Comment out active storage stuff, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :leagues, only: %i[index create] do
+    get 'find', on: :collection
+  end
 end

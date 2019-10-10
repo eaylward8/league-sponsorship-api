5a56357f0f8d00cc0a46f01e84b05e3845147807
Erik Aylward <eaylward@mail.med.upenn.edu>
2019-10-10 11:14:06 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+League.create(name: 'The Wyld Stallions', lat: 1, lng: 3, price: 4500)
+League.create(name: 'Team Zoidberg', lat: 4, lng: 2, price: 6000)
+League.create(name: 'The Zoomers', lat: -3, lng: -1, price: 1500)
+League.create(name: 'North Horseburg Little League', lat: 2, lng: 5, price: 3500)
+League.create(name: 'The Duloc Ogres', lat: -4, lng: 1, price: 2500)
+
+League.create(name: 'South Parkland Youth Association', lat: 10, lng: 2, price: 3000)
+League.create(name: 'Emmaus Youth Association', lat: 5, lng: 7, price: 2000)
+League.create(name: 'Hulmeville Soccer Club ', lat: -5, lng: 2, price: 5000)
+League.create(name: 'Philly Sports Leagues', lat: 4, lng: -6, price: 750)
